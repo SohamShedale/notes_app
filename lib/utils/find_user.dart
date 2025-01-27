56cbf7c73746bfa66c11df8dc7fed8d70dd4d909
SohamShedale <shedalesoham@gmail.com>
2025-01-27 16:12:27 +0530

    refactor code

--- lib/utils/find_user.dart
+++ lib/utils/find_user.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class FindUser {
-  static Future<bool> findUser(List userData,
-      {required TextEditingController emailController,
-      required TextEditingController passwordController}) async {
-    try {
-      final SharedPreferences prefs = await SharedPreferences.getInstance();
-      final userEmail = prefs.getString("userEmail");
-      final userPassword = prefs.getString("userPassword");
-      if (userEmail == emailController.text &&
-          userPassword == passwordController.text) {
-        return true;
-      }
-      return false;
-    } catch (e) {
-      throw e.toString();
+Future<bool> findUser(
+    {required TextEditingController emailController,
+    required TextEditingController passwordController}) async {
+  try {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final userEmail = prefs.getString("userEmail");
+    final userPassword = prefs.getString("userPassword");
+    if (userEmail == emailController.text &&
+        userPassword == passwordController.text) {
+      return true;
     }
+    return false;
+  } catch (e) {
+    throw e.toString();
   }
 }
