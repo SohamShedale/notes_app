8257a0d3bfcfaf9fe1e0ba5d195c4fec78f168ba
SohamShedale <shedalesoham@gmail.com>
2025-01-28 17:14:10 +0530

    add drawer widget

--- lib/pages/data_list.dart
+++ lib/pages/data_list.dart
@@ -1,6 +1,7 @@
 import 'package:authentication/services/database_service.dart';
 import 'package:authentication/utils/search_cards.dart';
 import 'package:authentication/utils/show_dialog_box.dart';
+import 'package:authentication/widgets/build_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:authentication/widgets/build_card.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
@@ -107,75 +108,7 @@ class _DataListState extends State<DataList> {
           ),
         ),
       ),
-      drawer: Drawer(
-        backgroundColor: Color(0xff202124),
-        child: ListView(
-          children: [
-            ListTile(
-              title: const Text(
-                'Notes App',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xffFFD166),
-                  fontSize: 30,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.lightbulb,
-                color: Colors.white,
-              ),
-              title: Text(
-                'My Notes',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.archive,
-                color: Colors.white,
-              ),
-              title: Text(
-                'Archive',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.delete,
-                color: Colors.white,
-              ),
-              title: Text(
-                'Trash',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.help,
-                color: Colors.white,
-              ),
-              title: Text(
-                'Help & feedback',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              onTap: () {},
-            ),
-          ],
-        ),
-      ),
+      drawer: BuildDrawer(),
       body: SafeArea(
         child: data != null
             ? (data!.isNotEmpty)
