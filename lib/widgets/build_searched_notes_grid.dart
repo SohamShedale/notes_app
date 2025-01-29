d31ad8edac00110ff807b83f987dbe09167405ae
SohamShedale <shedalesoham@gmail.com>
2025-01-29 17:28:46 +0530

    build grid for searched notes

---
+++ lib/widgets/build_searched_notes_grid.dart
@@ -0,0 +1,37 @@
+import 'package:authentication/constants/colors.dart';
+import 'package:authentication/providers/search_notes_provider.dart';
+import 'package:authentication/widgets/build_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:provider/provider.dart';
+
+class BuildSearchedNotesGrid extends StatelessWidget {
+  const BuildSearchedNotesGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final searchNotesProvider =
+        Provider.of<SearchNotesProvider>(context, listen: false);
+    return Column(
+      children: [
+        Expanded(
+          child: MasonryGridView.builder(
+            gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemCount: searchNotesProvider.searchedNotes.length,
+            itemBuilder: (context, index) {
+              final toDisplayItems = searchNotesProvider.searchedNotes;
+              final item = toDisplayItems[index];
+              final color = colors[index % colors.length];
+              return BuildCard(
+                item: item,
+                color: color,
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
