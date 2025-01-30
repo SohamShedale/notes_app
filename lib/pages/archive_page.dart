ffae5194343485a7c046e879eda3b8e24cef96aa
SohamShedale <shedalesoham@gmail.com>
2025-01-30 12:31:10 +0530

    add menu items page

---
+++ lib/pages/archive_page.dart
@@ -0,0 +1,26 @@
+import 'package:authentication/widgets/build_drawer.dart';
+import 'package:flutter/material.dart';
+
+class ArchivePage extends StatelessWidget {
+  const ArchivePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff202124),
+        leading: Builder(builder: (context) {
+          return IconButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              icon: Icon(
+                Icons.menu,
+                color: Colors.white,
+              ));
+        }),
+      ),
+      drawer: BuildDrawer(),
+    );
+  }
+}
