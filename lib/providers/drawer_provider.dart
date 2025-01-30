21451cddcf207b29ef032cd708b647e5599c8a8c
SohamShedale <shedalesoham@gmail.com>
2025-01-30 12:32:16 +0530

    build drawer menu provider

---
+++ lib/providers/drawer_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DrawerProvider extends ChangeNotifier {
+  int _selectedIndex = 0;
+  int get selectedIndex => _selectedIndex;
+  void updateSelectedIndexint(index) {
+    _selectedIndex = index;
+    notifyListeners();
+  }
+}
