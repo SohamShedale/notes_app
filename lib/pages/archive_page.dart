5cef08149a480f757a87fe776748078357e3f927
SohamShedale <shedalesoham@gmail.com>
2025-01-31 14:23:36 +0530

    display archived notes

--- lib/pages/archive_page.dart
+++ lib/pages/archive_page.dart
@@ -1,5 +1,9 @@
+import 'package:authentication/providers/notes_provider.dart';
+import 'package:authentication/widgets/build_archived_notes_grid.dart';
 import 'package:authentication/widgets/build_drawer.dart';
+import 'package:authentication/widgets/build_text.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ArchivePage extends StatelessWidget {
   const ArchivePage({super.key});
@@ -11,16 +15,30 @@ class ArchivePage extends StatelessWidget {
         backgroundColor: Color(0xff202124),
         leading: Builder(builder: (context) {
           return IconButton(
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-              icon: Icon(
-                Icons.menu,
-                color: Colors.white,
-              ));
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            icon: Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+          );
         }),
       ),
       drawer: BuildDrawer(),
+      body: SafeArea(
+        child:
+            Consumer<NotesProvider>(builder: (context, notesProvider, child) {
+          List<Map<String, dynamic>> archivedNotes = notesProvider.notes
+              .where((note) => (note["status"] == "archived"))
+              .toList();
+          return (archivedNotes.isNotEmpty)
+              ? BuildArchivedNotesGrid()
+              : Center(
+                  child: BuildText(text: "No archived notes"),
+                );
+        }),
+      ),
     );
   }
 }
